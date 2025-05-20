792c914d0b56cd2410049368f110df3312931289
Pankaj <ydvpnkj28@gmail.com>
2025-05-20 22:55:20 +0530

    class example

--- classEx.py
+++ classEx.py
@@ -1,7 +1,19 @@
-class Person:
+#this is the simple way to crate class and provide attributes
+'''class Person:
     name = 'Pankaj'
     age = 26
 
 p = Person
 print(p.name)
-print(p.age)
+print(p.age)'''
+
+#creating class with constructor
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person('Pankaj', 26)
+
+print(p1.name)
+print(p1.age)
