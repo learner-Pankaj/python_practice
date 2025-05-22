cfa66345e167cfe034ad390309d7819766dc4acd
Pankaj <ydvpnkj28@gmail.com>
2025-05-22 21:41:52 +0530

    tuple example

--- tupEx.py
+++ tupEx.py
@@ -8,6 +8,6 @@ print(tup)
 #tuple can't be updated
 #tuple can only be updated in the way is convert it into list and update the tuple and again convert it into tuple and print
 coordinate = (10,20)
-(x, y) = coordinate
+(x, y) = coordinate #remeber always number of argument should be the same if not * have to be used
 print(x)
 print(y)
