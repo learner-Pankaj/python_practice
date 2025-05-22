40d021cc952c161866fca24c3cc3071d3662721d
Pankaj <ydvpnkj28@gmail.com>
2025-05-22 21:41:13 +0530

    class example

--- classEx.py
+++ classEx.py
@@ -12,8 +12,22 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def greet(self):
+        print(f"Hi, I am {self.name} and I am {self.age} years old.")
 
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+    
+    def greet(self):
+        print(f"Hi, I am {self.name} and I am {self.age} years old and my salary is {self.salary}")
+    
 p1 = Person('Pankaj', 26)
-
+p1.greet()
 print(p1.name)
 print(p1.age)
+
+e1 = Employee('Pankaj', 26, 1234567)
+e1.greet()
